3aefe4426e64c9d05fc93c7bcb15117e1b6fed31
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-05-22 10:35:22 +0200

    add strdup

---
+++ ft_strdup.c
@@ -0,0 +1,33 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 10:34:01 by iisaacs           #+#    #+#             */
+/*   Updated: 2019/05/22 10:34:42 by iisaacs          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+
+void	*ft_strdup(const char *s1)
+{
+	char	*new_s;
+	int		len;
+	int		i;
+
+	i = 0;
+	len = strlen(s1);
+	new_s = (char *)malloc(len + 1);
+	while (len > 0)
+	{
+		new_s[i] = s1[i];
+		len--;
+		i++;
+	}
+	new_s[i] = '\0';
+	return ((void *)new_s);
+}
