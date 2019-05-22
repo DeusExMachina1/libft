05219c0676d1e96a1ddff11731037a7fe4fb0f34
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-05-22 12:43:54 +0200

    add memmove

---
+++ ft_memmove.c
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/22 12:41:53 by iisaacs           #+#    #+#             */
+/*   Updated: 2019/05/22 12:43:10 by iisaacs          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <string.h>
+#include <stdlib.h>
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	char	*n_dst;
+	char	*n_src;
+	char	*temp;
+	int		i;
+
+	n_dst = (char *)dst;
+	n_src = (char *)src;
+	temp = (char *)malloc(sizeof(src) + 1);
+	i = -1;
+	while (++i < len)
+		temp[i] = n_src[i];
+	while (len > 0)
+	{
+		*n_dst = *temp;
+		n_dst++;
+		temp++;
+		len--;
+	}
+	return (dst);
+}
