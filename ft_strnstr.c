38acc0549cd348eafba7f4eff22a2c7dbe2c68b7
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-05-23 16:39:53 +0200

    update functions

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 13:47:46 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/05/23 10:47:58 by iisaacs          ###   ########.fr       */
+/*   Updated: 2019/05/23 16:39:25 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,29 +14,29 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	int i;
-	int j;
+	int hay_len;
+	int hay_i;
+	int n_i;
+	int n_len;
 
-	i = -1;
-	j = 0;
-	while (haystack[++i] && len > 0)
+	hay_i = -1;
+	n_i = 0;
+	hay_len = (int)ft_strlen(haystack);
+	n_len = (int)ft_strlen(needle);
+	if (!needle[n_i])
+		return ((char *)haystack);
+	while (++hay_i < hay_len && len > 0)
 	{
-		if (haystack[i] == needle[j])
-		{
-			while (haystack[i] && len > 0 && needle != '\0')
-			{
-				if (haystack[i] != needle[j])
-					break ;
-				i++;
-				j++;
-				len--;
-			}
-			if (needle[j] == '\0')
-				return ((char *)&haystack[i - j]);
-			else
-				j = 0;
-		}
 		len--;
+		while (haystack[hay_i] == needle[n_i] && len > 0)
+		{
+			hay_i++;
+			n_i++;
+			len--;
+			if (n_i == n_len)
+				return ((char *)&haystack[hay_i - n_i]);
+		}
+		n_i = 0;
 	}
 	return (NULL);
 }
--- ft_strstr.c
+++ ft_strstr.c
@@ -6,41 +6,35 @@
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/21 09:40:49 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/05/23 15:16:21 by iisaacs          ###   ########.fr       */
+/*   Updated: 2019/05/23 16:30:12 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
 char	*ft_strstr(const char *haystack, const char *needle)
 {
-	int len;
+	int hay_len;
 	int hay_i;
 	int n_i;
+	int n_len;
 
-	hay_i = 0;
+	hay_i = -1;
 	n_i = 0;
-	len = (int)ft_strlen(haystack);
-	while (hay_i < len)
+	hay_len = (int)ft_strlen(haystack);
+	n_len = (int)ft_strlen(needle);
+	if (!needle[n_i])
+		return ((char *)haystack);
+	while (++hay_i < hay_len)
 	{
 		while (haystack[hay_i] == needle[n_i])
 		{
-			if (n_i == (int)ft_strlen(needle))
-				return ((char *)&haystack[hay_i - n_i]);
 			hay_i++;
 			n_i++;
+			if (n_i == n_len)
+				return ((char *)&haystack[hay_i - n_i]);
 		}
 		n_i = 0;
-		hay_i++;
 	}
 	return (NULL);
-}
-
-int		main(void)
-{
-	char *str1 = "The all and powerfull Noodle Monster";
-	char *str2 = "Noodle";
-	printf("%s", ft_strstr(str1, str2));
-	return (0);
 }
