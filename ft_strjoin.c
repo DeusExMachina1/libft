d55c44248fab88c553d45540a3f3a323331f5c27
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-05-30 14:36:23 +0200

    update library

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,35 +1,44 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strsub.c                                        :+:      :+:    :+:   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/29 14:57:40 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/05/30 08:27:33 by iisaacs          ###   ########.fr       */
+/*   Created: 2019/05/30 08:29:03 by iisaacs           #+#    #+#             */
+/*   Updated: 2019/05/30 09:04:23 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include <stdio.h>
 
-char	*ft_strsub(const char *s, unsigned int start, size_t len)
+char	*ft_strjoin(char const *s1, char const *s2)
 {
-	int		i;
 	char	*new_str;
+	int		len;
+	int		i;
+	int		j;
+
 
 	i = 0;
+	j = 0;
+	len = ft_strlen(s1) + ft_strlen(s2);
 	new_str = (char *)malloc(len);
 	if (!new_str)
 		return (NULL);
-	ft_strncpy(new_str, &s[start], len);
+	new_str = ft_strcat((char *)s1, s2);
 	return (new_str);
 }
 
-/*int		main(void)
+int		main(void)
 {
-	char *s = "STRING ";
-	char *ns = "CONCATED";
-	printf("%s", ft
+	char str1[30] = "sugar, spice";
+	char *str2 = " everything nice";
+	char *f_str; 
+	printf("size of s1: %lu\n", ft_strlen(str1)); 
+	printf("size of s2: %lu\n", ft_strlen(str2)); 
+	f_str = ft_strjoin(str1, str2);
+	printf("Joined string: %s", f_str);
 	return (0);
-}*/
+}
--- ft_strtrim.c
+++ ft_strtrim.c
@@ -1,27 +1,37 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strcpy.c                                        :+:      :+:    :+:   */
+/*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/21 11:19:58 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/05/30 09:16:08 by iisaacs          ###   ########.fr       */
+/*   Created: 2019/05/30 09:05:25 by iisaacs           #+#    #+#             */
+/*   Updated: 2019/05/30 14:36:00 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strcpy(char *dst, const char *src)
+char	*ft_strtrim(char const *s)
 {
-	int i;
+	int		i;
+	int		first;
+	int		last;
+	char	*new_str;
 
 	i = 0;
-	while (src[i] != '\0')
-	{
-		dst[i] = src[i];
+	first = 0;
+	last = 0;
+	while (ft_isspace(s[i]))
 		i++;
-	}
-	dst[i] = '\0';
-	return (dst);
+	first = i;
+	if ((i + 1) == ft_strlen(s))
+		return (NULL);
+	i = ft_strlen(s) - 1;
+	while (ft_isspace(s[i]))
+		i--;
+	last = i;
+	new_str = (char *)malloc(last - first + 2);
+	new_str[last - first + 1] = '\0';
+	return (ft_strncpy(new_str, &s[first], (last - first + 1)));
 }
