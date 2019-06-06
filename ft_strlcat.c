f1adff40d798027b07d3bc0e466b783d55ae9e99
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-06-06 08:34:32 +0200

    add ft_strlcat.c

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,33 +6,26 @@
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/24 15:35:23 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/06/03 16:11:03 by iisaacs          ###   ########.fr       */
+/*   Updated: 2019/06/06 08:33:32 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-#include <stdio.h>
 
-size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
+size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
-	int len;
 	int i;
 	int j;
 
 	j = 0;
 	i = ft_strlen(dst);
-	len = (int)ft_strlen(dst);
-	while (dst[i + j] == '\0' && len <= dstsize)
+	if ((int)size < i + 1)
+		return (ft_strlen(src) + size);
+	while (src[j] != '\0' && (i + j + 1) < (int)size)
 	{
-		;
+		dst[i + j] = src[j];
+		j++;
 	}
-}
-
-int		main(void)
-{
-	char	str1[30] = "hi harry, ";
-	char	*str2 = "you are such a dick";
-	printf("result: %lu\n", strlcat(str1, str2, 11));
-	printf("dest res: %s**\n", str1);
-	return (0);
+	dst[i + j] = '\0';
+	return (i + ft_strlen(src));
 }
