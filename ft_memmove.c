ff21afd533e8526275c739711e6bcdba0b86d8de
Ismael ISAACS <iisaacs@c3r1s9.wethinkcode.co.za>
2019-06-12 16:33:27 +0200

    updated ft_memmove.c

--- ft_memmove.c
+++ ft_memmove.c
@@ -6,7 +6,7 @@
 /*   By: iisaacs <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/05/22 12:41:53 by iisaacs           #+#    #+#             */
-/*   Updated: 2019/05/23 17:44:51 by iisaacs          ###   ########.fr       */
+/*   Updated: 2019/06/12 16:33:02 by iisaacs          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,21 +16,23 @@ void	*ft_memmove(void *dst, const void *src, size_t len)
 {
 	char	*n_dst;
 	char	*n_src;
-	char	*temp;
 	int		i;
 
 	n_dst = (char *)dst;
 	n_src = (char *)src;
-	temp = (char *)malloc(sizeof(src) + 1);
-	i = -1;
-	while (++i < (int)len)
-		temp[i] = n_src[i];
-	while (len > 0)
+	i = 0;
+	if (!dst && !src)
+		return (NULL);
+	if (src < dst)
 	{
-		*n_dst = *temp;
-		n_dst++;
-		temp++;
-		len--;
+		while (++i <= (int)len)
+			n_dst[len - i] = n_src[len - i];
+	}
+	else
+	{
+		i = -1;
+		while (++i < (int)len)
+			n_dst[i] = n_src[i];
 	}
 	return (dst);
 }
